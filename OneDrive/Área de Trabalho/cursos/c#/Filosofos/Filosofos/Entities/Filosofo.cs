50a9de2515b3cf6c2e7c9c974104a15e3391d3ac
maria <marianyf76@gmail.com>
2026-04-07 17:49:15 -0300

    alteração

---
+++ OneDrive/Área de Trabalho/cursos/c#/Filosofos/Filosofos/Entities/Filosofo.cs
@@ -0,0 +1,70 @@
+﻿namespace Filosofos.Entities
+{
+    internal class Filosofo
+    {
+        public enum Estado
+        {
+            Comendo,
+            Pensando
+        }
+        public string Nome { get; set; }
+        public bool Faca { get; private set; }
+        public bool Garfo { get; private set; }
+        public int Comida { get; private set; }
+        public Estado EstadoAtual
+        {
+            get
+            {
+                return (Faca && Garfo && Comida > 0) ?  Estado.Comendo : Estado.Pensando;
+            }
+        }
+
+        public Filosofo(string nome, int comida)
+        {
+            Nome = nome;
+            Faca = false;
+            Garfo = false;
+            Comida = comida;
+        }
+
+        public bool PegarFaca()
+        {
+            Faca = true;
+            return true;
+        }
+        public bool PegarGarfo()
+        {
+            Garfo = true;
+            return true;
+        }
+        public bool LargarFaca()
+        {
+            Faca = false;
+            return true;
+        }
+        public bool LargarGarfo()
+        {
+            Garfo = false;
+            return true;
+
+        }
+
+        public void Comer()
+        {
+            if (Garfo && Faca && Comida > 0)
+            {
+                Comida--;
+                Console.WriteLine("Comendo... comida restante: " + Comida);
+            }
+            else
+            {
+                Console.WriteLine("Comida indisponivel.");
+            }
+        }
+        public void MostrarEstado()
+        {
+            Console.WriteLine($"{Nome} -> Estado: {EstadoAtual.ToString()} | Comida: {Comida}");
+        }
+
+    }
+}
---
+++ OneDrive/Área de Trabalho/cursos/c#/Filosofos/Filosofos/Program.cs
@@ -0,0 +1,69 @@
+﻿using Filosofos.Entities;
+
+namespace Filosofos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Filosofo f1 = new Filosofo("Filósofo 1", 3);
+            Filosofo f2 = new Filosofo("Filósofo 2", 2);
+
+            while (true)
+            {
+                Console.Clear();
+
+                f1.MostrarEstado();
+                f2.MostrarEstado();
+
+                Console.WriteLine("Escolha uma opção:");
+                Console.WriteLine("1 - Filósofo 1 comer");
+                Console.WriteLine("2 - Filósofo 2 comer");
+                Console.WriteLine("0 - Sair");
+
+                Console.Write("Opção: ");
+                int escolha = int.Parse(Console.ReadLine());
+
+                if (escolha == 0)
+                    break;
+
+                if (escolha == 1 || escolha == 2)
+                {
+                    Filosofo f = escolha == 1 ? f1 : f2;
+
+                    ExecutarCiclo(f);
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida!");
+                }
+
+                if (f1.Comida <= 0 && f2.Comida <= 0)
+                {
+                    Console.WriteLine("A comida de todos acabou!");
+                    break;
+                }
+
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Fim da simulação.");
+        }
+
+        static void ExecutarCiclo(Filosofo f)
+        {
+            f.PegarGarfo();
+            Console.WriteLine($"{f.Nome} pegou o garfo.");
+            f.PegarFaca();
+            Console.WriteLine($"{f.Nome} pegou a faca.");
+
+            f.Comer();
+
+            f.LargarGarfo();
+            Console.WriteLine($"{f.Nome} largou o garfo.");
+            f.LargarFaca();
+            Console.WriteLine($"{f.Nome} largou a faca.");
+        }
+    }
+}
